4581767b6d1dc1e0370862bb550da033b6898592
André Neder <144075267+dreneder@users.noreply.github.com>
2025-12-09 13:20:54 -0500

    changes on JS, HTML for data entry, parameters also added to the python

--- CART 351 Project 3 (WEBSITE BASE version 2)/journalMoodAnalyzer.py
+++ CART 351 Project 3 (WEBSITE BASE version 2)/journalMoodAnalyzer.py
@@ -1,9 +1,21 @@
 #Importing and setting up the python document
 from flask import Flask, render_template, Response, request
+from flask_pymongo import PyMongo
 import json
 import os
+from datetime import datetime
+
 
 app = Flask(__name__)
+app.secret_key = os.getenv("FLASK_SECRET_KEY", "dev_secret")
+
+db_user = os.getenv("MONGODB_USER")
+db_pass = os.getenv("DATABASE_PASSWORD")
+db_name = os.getenv("DATABASE_NAME")
+app.config["MONGO_URI"] = (
+     f"mongodb+srv://{db_user}:{db_pass}@cluster0.6ifd1w5.mongodb.net/{db_name}?retryWrites=true&w=majority"
+)
+mongo = PyMongo(app)
 
 UPLOAD_FOLDER = 'static/uploads' 
 
@@ -16,7 +28,7 @@ def home():
       return render_template("home.html")
 
 #The journal entry submit route
-@app.route("/submitEntry")
+@app.route("/submitEntry", methods=["GET", "POST"])
 def submitEntry():
      return render_template("submitEntry.html")
 
--- CART 351 Project 3 (WEBSITE BASE version 2)/static/js/formScript.js
+++ CART 351 Project 3 (WEBSITE BASE version 2)/static/js/formScript.js
@@ -4,22 +4,21 @@ window.onload = function () {
 
     const startContainer = this.document.getElementById("start-container");
     const formContainer = this.document.getElementById("form-container");
+    const resultContainer = this.document.getElementById("form-result");
     const startButton = this.document.getElementById("start-button");
 
     // const startButton = this.document.querySelector("start-button")
     
     if (formContainer) formContainer.style.display = "none";
+    if (resultContainer) resultContainer.style.display = "none";
 
-
-    
+    // start button logic can be used to write the first or more entries
     if (startButton) {
-        // formContainer.style.display = "none";
         startButton.addEventListener("click", () => {
             startContainer.style.display = "none";
-            startButton.style.display = "none";
+            resultContainer.style.display = "none";
             formContainer.style.display = "";
         });
-        
     };
 
 }
